ebc798e44ea2216fe9aa82d08844ce171db15752
Nabavi2 <rohullah.ghaznavi78@gmail.com>
2021-12-20 21:50:42 +0430

    changed card and headerbutton code in UI folder

--- app2/components/UI/Card.js
+++ app2/components/UI/Card.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
 
-const Card = props => {
-  return <View style={{...styles.card, ...props.style}}>{props.children}</View>;
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
 };
 
 const styles = StyleSheet.create({
   card: {
-    shadowColor: 'black',
+    shadowColor: "black",
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: 'white'
-  }
+    backgroundColor: Colors.white,
+  },
 });
 
 export default Card;
