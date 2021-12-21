1f848db206b04b212bbfa1e9a6fd68e70f9d99d7
Nabavi2 <rohullah.ghaznavi78@gmail.com>
2021-12-21 11:07:21 +0430

    changed threa file in UI of Component

--- app/components/UI/Card.js
+++ app/components/UI/Card.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-
-const Card = props => {
-  return <View style={{...styles.card, ...props.style}}>{props.children}</View>;
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/Colors";
+const Card = (props) => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
 };
 
 const styles = StyleSheet.create({
   card: {
-    shadowColor: 'black',
+    shadowColor: Colors.black,
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: 'white'
-  }
+    backgroundColor: Colors.white,
+  },
 });
 
 export default Card;
